3c49ea3541b50f118cdfc65ccbebe356f980508f
Filipe Cruz <bad-alloc-error@yandex.com>
2022-07-30 04:19:32 -0300

    primeira implementação

--- boot.asm
+++ boot.asm
@@ -1,15 +1,36 @@
-ORG 0x7c00
+ORG 0x7c00 
 BITS 16
 
 start:
 
-    mov ah, 0eh
-    mov al, 'A'
-    mov bx, 0
-    int 0x10
+    mov si, message     ; move para o reg o primeiro char da string
+    call print
+    jmp $               ; loop que itera char por char
 
-times 510-($ - $$) db 0
-dw 0xAA55
+print:
+    mov bx, 0
+
+.loop:
+    lodsb               ; carrega o char que o reg si esta apontando
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+
+.done:    
+    ret
+
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
+
+message: db 'smallOS Bootloader', 0
+
+times 510-($ - $$) db 0 ; preenche os bytes restantes com zero
+dw 0xAA55               ; intel é little endianess
+
+
     
 
 
